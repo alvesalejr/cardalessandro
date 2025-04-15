06e941fc0a3bf18f677bb2511d537315f8529e90
Alessandro Junior <alvesalejr@gmail.com>
2025-04-15 19:34:03 -0300

    update portfolio

--- src/script.js
+++ src/script.js
@@ -1,4 +1,22 @@
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtn = document.getElementById("openCurriculo")
+  const modal = document.getElementById("pdfModal")
+  const closeBtn = document.getElementById("closeModal")
+
+  if (openBtn && modal && closeBtn) {
+    openBtn.addEventListener("click", function (e) {
+      e.preventDefault()
+      modal.style.display = "block"
+    })
+    closeBtn.addEventListener("click", function () {
+      modal.style.display = "none"
+    })
+    window.addEventListener("click", function (event) {
+      if (event.target === modal) {
+        modal.style.display = "none"
+      }
+    })
+  }
+})
+
+
